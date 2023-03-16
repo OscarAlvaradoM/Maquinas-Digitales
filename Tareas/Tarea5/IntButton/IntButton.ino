241f8b8ec9815329e392cc9e563a89ffbd83b0da
Óscar Alvarado <OscarAlvarado@ciencias.unam.mx>
2023-03-16 11:42:00 -0600

    Se agrega el programa de las interrupciones con el botón

---
+++ Tareas/Tarea5/IntButton/IntButton.ino
@@ -0,0 +1,21 @@
+#define PIN_BUTTON 34
+int numBotPres = 0;
+uint32_t lastMillis = 0;
+
+void IRAM_ATTR isrButton() {
+  // Código para evitar el bouncing
+  if (millis() - lastMillis > 10) {
+    numBotPres += 1;
+    lastMillis = millis();
+  }
+}
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(PIN_BUTTON, INPUT);
+    attachInterrupt(PIN_BUTTON, isrButton, RISING);
+}
+
+void loop() {
+    Serial.printf("El botón se ha presionado %u veces\n", numBotPres);
+}
